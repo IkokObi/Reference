4befed91f353432d1d9a6819df4803a12a94099b
Oohikata <koki.obi.321@gmail.com>
2018-11-03 23:33:54 +0900

    [Rename] function name

--- Python_Library_Import/lib/caller.py
+++ Python_Library_Import/lib/caller.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on : 2018/10/30 14:17:36 JST.
-Last Change: 2018/10/30 14:34:22 JST. 
+Last Change: 2018/11/03 23:31:43 JST.
 
 @author: Koki Obinata
 """
 
-import lib.hello as test
-from lib.deep_lib.deep_hello import deep_hello 
+import lib.hello as hello
+from lib.deep_lib.deep_hello import deep_hello
+
 
 def call_hello():
     print('Hello from hello.py')
-    test.hello()
+    hello.hello()
+
 
 def call_deep_hello():
     print('Hello from deep_lib.deep_hello.py')
