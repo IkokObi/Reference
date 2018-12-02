31d98ebdcd74075254d1cab72d25b906fbeca57e
Oohikata <koki.obi.321@gmail.com>
2018-12-03 02:58:21 +0900

    [Add] confusion matrix template
    混合行列

---
+++ Python/matplotlib/confusion_matrix.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on : 2018/11/29 14:30:09 JST.
+Last Change: 2018/12/03 02:08:44 JST.
+
+@author: Koki Obinata
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
+
+
+def print_cmx(y_true, y_pred, labels, name_labels, name):
+    """ 混合行列のプロット """
+    # 混合行列を作成, c_ij は真のグループが i で予測グループが j の数を表す
+    cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
+
+    # プロット用のpd.DataFrame型
+    df_cmx = pd.DataFrame(cmx_data, index=name_labels, columns=name_labels)
+
+    plt.figure(figsize=(8, 6))
+    sn.heatmap(df_cmx, annot=True, fmt='d', cmap='Blues')
+    plt.show()
+    # plt.savefig(name)
+
+
+if __name__ == '__main__':
+    y_true = np.array([1, 1, 2, 3, 1, 1, 2, 2, 3, 2, 1])
+    y_pred = np.array([1, 2, 2, 3, 2, 1, 2, 3, 3, 2, 3])
+    labels = [1, 2, 3]
+    name_labels = ['spam', 'ham', 'egg']
+    # name_labels = labels
+    name = 'confusion_matrix'
+    print_cmx(y_true, y_pred, labels, name_labels, name)
