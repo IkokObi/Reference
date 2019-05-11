a5930ac4b3fbf2b179fd0aa51c4f40f47b7f339c
Oohikata <koki.obi.321@gmail.com>
2019-05-11 20:57:51 +0900

    Initial Commit
    hackers barでの初コミット

---
+++ Python/hackers_bar/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on : 2019/05/11 20:55:42 JST.
+Last Change: 2019/05/11 20:55:54 JST.
+
+@author: Koki Obinata
+"""
+print('Helo World!')
+
