ada195a5fbd2e5b6687b24d2c5c4b830c11b0bfd
Oohikata <koki.obi.321@gmail.com>
2019-05-11 21:01:19 +0900

    Update test.py
    Add one more print

--- Python/hackers_bar/test.py
+++ Python/hackers_bar/test.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on : 2019/05/11 20:55:42 JST.
-Last Change: 2019/05/11 20:55:54 JST.
+Last Change: 2019/05/11 20:59:29 JST.
 
 @author: Koki Obinata
 """
 print('Helo World!')
 
+print('This is excellent code!')
